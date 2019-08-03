35ebe397856858c24faf9f88d0e5f5dac12c77cc
Benjamin Bollen <ben@ost.com>
2019-08-03 11:30:27 +0530

    generate multiple keys from seed phrase move to services

--- src/services/MnemonicPrivateKeys.js
+++ src/services/MnemonicPrivateKeys.js
@@ -7,25 +7,34 @@ const keythereum = require('keythereum');
 
 exports.debug = false;
 
-function getPrivateKeyFromMnemonic(mnemonic) {
+function getPrivateKeysFromMnemonic(mnemonic, number) {
 	const hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
 	const wallet_hdpath = "m/44'/60'/0'/0/";
 
-	const wallet = hdwallet.derivePath(wallet_hdpath + '0').getWallet();
-	const privateKey = wallet._privKey.toString('hex');
-	const account = '0x' + wallet.getAddress().toString('hex').toUpperCase();
+  let keyPairs = [];
+  for (let i = 0; i < number; i++) {
+    const wallet = hdwallet.derivePath(wallet_hdpath + i.toString()).getWallet();
+    const privateKey_ = wallet._privKey.toString('hex');
+    const account_ = '0x' + wallet.getAddress().toString('hex').toUpperCase();
+    
+    if (exports.debug)
+      console.log({
+        mnemonic: mnemonic,
+        privateKey: privateKey_,
+        account: account_
+    });
+    
+    const keyPair_ = {
+      privateKey: privateKey_,
+      account: account_
+    }
+    keyPairs.push(keyPair_);
+  }
 
-	if (exports.debug)
-		console.log({
-			mnemonic: mnemonic,
-			privateKey: privateKey,
-			account: account
-		});
-
-	return privateKey;
+	return keyPairs;
 }
 
-exports.getPrivateKeyFromMnemonic = getPrivateKeyFromMnemonic;
+exports.getPrivateKeysFromMnemonic = getPrivateKeysFromMnemonic;
 
 function getPrivateKeyFromRandom() {
 	const params = { keyBytes: 32, ivBytes: 16 };
