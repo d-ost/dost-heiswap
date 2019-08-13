62da8207c4124e26f6bd2318a3def7476d76100b
abhayks1 <abhay@ost.com>
2019-08-13 19:02:25 +0530

    Create and Verify pin screens

--- src/components/v2/CreatePin.tsx
+++ src/components/v2/CreatePin.tsx
@@ -7,16 +7,83 @@ interface Props {
 }
 
 interface State {
-
+  pin: string;
+  confirmPin: string;
+  errors: {pin: string, confirmPin: string}
 }
 
 export default class CreatePin extends Component<Props, State> {
+  
+  PIN_MINIMUM_LENGTH = 6;
+  
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      pin: '',
+      confirmPin: '',
+      errors: {pin: 'false', confirmPin: 'false'},
+    };
+    this.handlePinChange = this.handlePinChange.bind(this);
+    this.handleConfirmPinChange = this.handleConfirmPinChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handlePinChange(e) {
+    this.setState({
+        pin: e.target.value,
+    });
+  }
+
+  handleConfirmPinChange(e) {
+    this.setState({
+      confirmPin: e.target.value,
+    })
+  }
+
+  clearValidationErrors() {
+    this.setState({
+      errors: {pin: 'false', confirmPin: 'false'},
+    });
+  }
+
+  validatePin() {
+    console.log('validating pin...');
+    if(this.state.pin.length < this.PIN_MINIMUM_LENGTH) {
+      let errors = this.state.errors;
+      errors.pin = `Pin length should be greater or equal to ${this.PIN_MINIMUM_LENGTH}`;
+      this.setState({
+        errors: errors,
+      });
+    }
+
+    if(this.state.confirmPin.length < this.PIN_MINIMUM_LENGTH){
+      let errors = this.state.errors;
+      errors.confirmPin = `Pin length should be greater or equal to ${this.PIN_MINIMUM_LENGTH}`;
+      this.setState({
+        errors: errors,
+      });
+    }
+
+    if (this.state.pin !== this.state.confirmPin) {
+      let errors = this.state.errors;
+      errors.confirmPin = 'Pin mismatch!!!';
+      this.setState({
+        errors: errors,
+      });
+    }
+
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.clearValidationErrors();
+
+    const { pin, confirmPin } = this.state;
+    this.validatePin();
+    // Return if errors else store the pin
+    console.log(`pin: ${pin}, confirm pin: ${confirmPin}`);
+  }
 
   componentDidMount() {
   }
@@ -34,16 +101,36 @@ export default class CreatePin extends Component<Props, State> {
       <div className='CreatePin'>
         <Form>
           <Form.Group controlId="formPin">
-            <Form.Label>Create your Pin</Form.Label>
-            <Form.Control type="password" placeholder="Enter your Pin" minLength={6} required={true}/>
+            <Form.Label className='pin-label'>Create your Pin</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter your Pin"
+              minLength={this.PIN_MINIMUM_LENGTH}
+              required={true}
+              onChange={this.handlePinChange}
+              isInvalid={this.state.errors.pin !== 'false'}
+            />
+            <Form.Control.Feedback type="invalid">
+              {this.state.errors.pin !== 'false' && this.state.errors.pin}
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group controlId="formConfirmPin">
-            <Form.Label>Confirm Pin</Form.Label>
-            <Form.Control type="password" placeholder="Confirm your Pin" minLength={6} required={true}/>
+            <Form.Label className='pin-label'>Confirm Pin</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Confirm your Pin"
+              minLength={this.PIN_MINIMUM_LENGTH}
+              required={true}
+              onChange={this.handleConfirmPinChange}
+              isInvalid={this.state.errors.confirmPin !== 'false'}
+            />
+            <Form.Control.Feedback type="invalid">
+              {this.state.errors.confirmPin !== 'false' && this.state.errors.confirmPin}
+            </Form.Control.Feedback>
           </Form.Group>
 
-          <Button variant="primary" type="submit">
+          <Button variant="primary" onClick={this.handleSubmit}>
             Create Pin
           </Button>
         </Form>
--- src/components/v2/VerifyPin.tsx
+++ src/components/v2/VerifyPin.tsx
@@ -7,16 +7,56 @@ interface Props {
 }
 
 interface State {
-
+  pin: string,
+  errors: {pin: string},
 }
 
 export default class VerifyPin extends Component<Props, State> {
+
+  PIN_MINIMUM_LENGTH = 6;
+
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      pin: '',
+      errors: {pin: 'false'},
+    };
+    this.handlePinChange = this.handlePinChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handlePinChange(e) {
+    this.setState({
+      pin: e.target.value,
+    });
+  }
+
+  clearValidationErrors() {
+    this.setState({
+      errors: {pin: 'false'},
+    });
+  }
+
+  validatePin() {
+    console.log('validating pin...');
+    if(this.state.pin.length < this.PIN_MINIMUM_LENGTH) {
+      let errors = this.state.errors;
+      errors.pin = `Pin length should be greater or equal to ${this.PIN_MINIMUM_LENGTH}`;
+      this.setState({
+        errors: errors,
+      });
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.clearValidationErrors();
+    this.validatePin();
+    const { pin } = this.state;
+    // Return if errors else store the pin
+    console.log(`pin: ${pin}`);
+  }
 
   componentDidMount() {
   }
@@ -34,10 +74,20 @@ export default class VerifyPin extends Component<Props, State> {
         <Form>
           <Form.Group controlId="formPin">
             <Form.Label>Enter your Pin for Verification</Form.Label>
-            <Form.Control type="password" placeholder="Enter your Pin" minLength={6} required={true}/>
+            <Form.Control
+              type="password"
+              placeholder="Enter your Pin"
+              minLength={6}
+              required={true}
+              onChange={this.handlePinChange}
+              isInvalid={this.state.errors.pin !== 'false'}
+            />
+            <Form.Control.Feedback type="invalid">
+              {this.state.errors.pin !== 'false' && this.state.errors.pin}
+            </Form.Control.Feedback>
           </Form.Group>
 
-          <Button variant="primary" type="submit">
+          <Button variant="primary" onClick={this.handleSubmit}>
             Verify Pin
           </Button>
         </Form>
