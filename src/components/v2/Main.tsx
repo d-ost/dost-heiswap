98f2b6e24a7f2a278dabb978848cf7e7020f683c
Sarvesh Jain <sarvesh@ost.com>
2019-08-13 11:06:06 +0530

    Sample card on main page

--- src/components/v2/Main.tsx
+++ src/components/v2/Main.tsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 interface Props {
 
@@ -28,7 +30,19 @@ export default class Main extends Component<Props, State> {
 
   render() {
 
-    return <h1>Main component</h1>
+    return (
+      <Card style={{width: '18rem'}}>
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of
+            the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    );
   }
 
 }
