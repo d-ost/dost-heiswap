7a64a98f2ca5c0a5b3bb31db3b11f00df78e243a
Sarvesh Jain <sarvesh@ost.com>
2019-08-23 19:30:45 +0530

    Added georli relayer URL

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -2,6 +2,6 @@ export const ORIGIN_GAS_PRICE = '0x3B9ACA00'; // 20 Gwei
 export const AUXILIARY_GAS_PRICE = '0x3B9ACA00'; // 1 Gwei
 export const BASETOKEN_TRANSFER_GAS = '21000';
 export const HEISWAP_GOERLI = '0xFD546C315e6b09ba6E647b424Ef94CA743a74379';
-export const HEISWAP_RELAYER_GOERLI = '';
+export const HEISWAP_RELAYER_GOERLI = 'http://3.248.54.171:6363/';
 export const HEISWAP_ROPSTEN = '0x8AAbE42EeCA45E040fab330fD24eA6746b832Ad2';
 export const HEISWAP_RELAYER_ROPSTEN = 'https://relayer.heiswap.exchange';
